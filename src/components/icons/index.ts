c29a164907fb5307181edef6dcff941345914c57
Alexey Valko <valko.alexey@gmail.com>
2022-06-19 11:25:37 +0300

    refactor: update icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,5 @@
 import IconSearch from '@/components/icons/IconSearch.vue';
-import IconHeart from '@/components/icons/IconHeart.vue';
 import IconDownload from '@/components/icons/IconDownload.vue';
 import IconSmallArrow from '@/components/icons/IconSmallArrow.vue';
 
-export default [IconSearch, IconHeart, IconDownload, IconSmallArrow];
+export default [IconSearch, IconDownload, IconSmallArrow];
