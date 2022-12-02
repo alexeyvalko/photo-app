9b9d1f92f5dc2770a8dd9bd022653cd02528c84d
Aliaksei Valko <60567379+alexeyvalko@users.noreply.github.com>
2022-12-02 14:12:05 +0300

    update: update photoList skeleton

--- src/components/Skeleton/PhotoListSkeleton.vue
+++ src/components/Skeleton/PhotoListSkeleton.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="skeleton-container">
-    <SkeletonCard v-for="card in props.cards" :key="card" />
-  </div>
+  <ThreeColumnsLayout>
+    <template #column-1>
+      <template v-for="card in props.cards" :key="card">
+        <SkeletonCard :ratio="ratioListOne[card - 1] || 1.2" />
+      </template>
+    </template>
+    <template #column-2>
+      <template v-for="card in props.cards" :key="card">
+        <SkeletonCard :ratio="ratioListTwo[card - 1] || 1.2" />
+      </template>
+    </template>
+    <template #column-3>
+      <template v-for="card in props.cards" :key="card">
+        <SkeletonCard :ratio="ratioListThree[card - 1] || 1.2" />
+      </template>
+    </template>
+  </ThreeColumnsLayout>
+  <TwoColumnsLayout>
+    <template #column-1>
+      <template v-for="card in props.cards" :key="card">
+        <SkeletonCard :ratio="ratioListOne[card - 1] || 1.2" />
+      </template>
+    </template>
+    <template #column-2>
+      <template v-for="card in props.cards" :key="card">
+        <SkeletonCard :ratio="ratioListTwo[card - 1] || 1.2" />
+      </template>
+    </template>
+  </TwoColumnsLayout>
 </template>
 
 <script setup lang="ts">
 import SkeletonCard from './SkeletonCard.vue';
+import ThreeColumnsLayout from '@/components/Columns/ThreeColumnsLayout.vue';
+import TwoColumnsLayout from '@/components/Columns/TwoColumnsLayout.vue';
 const props = defineProps<{
   cards: number;
 }>();
+
+const ratioListOne = [1.2, 0.8, 1, 0.7, 1, 1.2, 0.8];
+const ratioListTwo = [0.8, 1, 1.3, 1.2, 1.2, 1, 1.5];
+const ratioListThree = [1.7, 0.9, 1, 0.6, 1, 2, 0.5];
 </script>
 
 <style>
--- src/components/Skeleton/SkeletonCard.vue
+++ src/components/Skeleton/SkeletonCard.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="skeleton-card"></div>
+  <div
+    class="skeleton-card"
+    :style="` padding-top: calc(100% / ${ratio || 1.2});`"
+  ></div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  ratio: number;
+}>();
+console.log(props.ratio);
+</script>
 
 <style>
 .skeleton-card {
   width: 100%;
   height: 100%;
-  padding-top: calc(100% / 1.2);
+  /* padding-top: calc(100% / 1.2); */
   border-radius: var(--card-border-radius);
   background-color: var(--color-background-mute);
   background: var(--skeleton-gradient);
-
   background-size: 200% 200%;
   animation: animation 2s ease infinite;
 }
