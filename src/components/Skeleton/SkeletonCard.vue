7561916549f2091632c5b1d10951f268588a6db5
Aliaksei Valko <60567379+alexeyvalko@users.noreply.github.com>
2023-01-31 00:10:19 +0300

    fix: light theme skeleton bug

--- src/components/Skeleton/SkeletonCard.vue
+++ src/components/Skeleton/SkeletonCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="skeleton-card"
+    class="skeleton skeleton-card"
     :style="`padding-top: calc(100% / ${ratio || 1.2});`"
   ></div>
 </template>
@@ -11,35 +11,10 @@ defineProps<{
 }>();
 </script>
 
-<style>
+<style scoped>
 .skeleton-card {
   width: 100%;
   height: 100%;
   border-radius: var(--card-border-radius);
-  animation-name: animation-skeleton;
-  animation-duration: 1.6s;
-  animation-delay: 85ms;
-  animation-iteration-count: infinite;
-  animation-timing-function: cubic-bezier(0.34, 0, 0.21, 1);
-  background-attachment: fixed;
-  background-color: hsla(0, 0%, 100%, 0.12);
-  background-repeat: no-repeat;
-  background-image: linear-gradient(
-    120deg,
-    transparent 40%,
-    hsla(0, 0%, 100%, 0.12) 50%,
-    transparent 60%
-  );
-  background-size: 100vw 100vh;
-}
-
-@keyframes animation-skeleton {
-  0% {
-    background-position: -66vw 0;
-  }
-
-  to {
-    background-position: 66vw 0;
-  }
 }
 </style>
--- src/components/Skeleton/SkeletonLine.vue
+++ src/components/Skeleton/SkeletonLine.vue
@@ -2,35 +2,10 @@
   <div class="skeleton-line"></div>
 </template>
 
-<style>
+<style scoped>
 .skeleton-line {
   width: 100%;
   height: 18px;
   border-radius: var(--card-border-radius);
-  animation-name: animation-skeleton;
-  animation-duration: 1.6s;
-  animation-delay: 85ms;
-  animation-iteration-count: infinite;
-  animation-timing-function: cubic-bezier(0.34, 0, 0.21, 1);
-  background-attachment: fixed;
-  background-color: hsla(0, 0%, 100%, 0.12);
-  background-repeat: no-repeat;
-  background-image: linear-gradient(
-    120deg,
-    transparent 40%,
-    hsla(0, 0%, 100%, 0.12) 50%,
-    transparent 60%
-  );
-  background-size: 100vw 100vh;
-}
-
-@keyframes animation-skeleton {
-  0% {
-    background-position: -66vw 0;
-  }
-
-  to {
-    background-position: 66vw 0;
-  }
 }
 </style>
